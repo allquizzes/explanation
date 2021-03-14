3130129a38047f5954a486e02c16f9f20db5800b
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-03-14 14:54:14 +0530

    Update extra.js

--- script/extra.js
+++ script/extra.js
@@ -1,1 +1,9 @@
+var i = 0;
+const randomColor = Math.floor(Math.random()*360);
+do {
+    document.getElementsByTagName("details")[i].style.backgroundColor = "hsl(" + randomColor + ", 100% , 85%";
+
+  i++;
+}
+while (i < 100);  
 
